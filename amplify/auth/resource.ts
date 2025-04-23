1c6f3c8bceb6f314a5511f2310921f48c8d8a8b0
Stefan Axelsson <26811776+saxelsso@users.noreply.github.com>
2025-04-23 20:40:33 +0200

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,10 +1,11 @@
 import { defineAuth } from '@aws-amplify/backend';
+import { AuthResource } from '@aws-amplify/backend/plugin-types';
 
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-export const auth = defineAuth({
+export const auth: AuthResource = defineAuth({
   loginWith: {
     email: true,
   },
