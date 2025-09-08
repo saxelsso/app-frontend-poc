d51e26079fc64df05eb1dddb2ec311cd976bf54f
Stefan Axelsson <26811776+saxelsso@users.noreply.github.com>
2025-09-08 10:21:16 +0200

    tweak tsconfig.json for frontend build part 8

--- amplify/functions/echo-service/handler.ts
+++ amplify/functions/echo-service/handler.ts
@@ -1,10 +1,37 @@
+import { Amplify } from 'aws-amplify';
 import type { Schema } from "../../data/resource"
 import { env } from "$amplify/env/echoService";
+import { generateClient } from "aws-amplify/data";
+
+Amplify.configure(
+    {/* resource configuration */},
+    {
+        Auth: {
+            credentialsProvider: {
+                // instruct the client library to read credentials from the environment
+                getCredentialsAndIdentityId: async () => ({
+                    credentials: {
+                        accessKeyId: env.AWS_ACCESS_KEY_ID,
+                        secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+                        sessionToken: env.AWS_SESSION_TOKEN,
+                    },
+                }),
+                clearCredentialsAndIdentityId: () => {
+                    /* noop */
+                },
+            },
+        },
+    }
+);
+
+const client = generateClient<Schema>();
 
 export const handler: Schema["echoService"]["functionHandler"] = async (event) => {
     // arguments typed from `.arguments()`
     const { echoString } = event.arguments
-    // return typed from `.returns()`
-    const awsRegion = env.AWS_REGION
-    return `Hello, ${echoString}! ` + awsRegion;
+
+    const todos = await client.models.Todo.list() // use this Data client for CRUDL requests
+    console.log("Todos in the database:", todos);
+
+    return `Hello, ${echoString}!`
 }
