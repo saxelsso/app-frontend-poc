0727fd4c214ba82071d12ab0f8cebe63eee6544d
Stefan Axelsson <26811776+saxelsso@users.noreply.github.com>
2025-09-08 12:26:59 +0200

    refactor: simplify Amplify configuration in echo service handler

--- amplify/functions/echo-service/handler.ts
+++ amplify/functions/echo-service/handler.ts
@@ -2,26 +2,10 @@ import { Amplify } from 'aws-amplify';
 import type { Schema } from "../../data/resource"
 import { env } from "$amplify/env/echoService";
 import { generateClient } from "aws-amplify/data";
+import amplifyOutputs from '../../../amplify_outputs.json';
 
 Amplify.configure(
-    {/* resource configuration */},
-    {
-        Auth: {
-            credentialsProvider: {
-                // instruct the client library to read credentials from the environment
-                getCredentialsAndIdentityId: async () => ({
-                    credentials: {
-                        accessKeyId: env.AWS_ACCESS_KEY_ID,
-                        secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-                        sessionToken: env.AWS_SESSION_TOKEN,
-                    },
-                }),
-                clearCredentialsAndIdentityId: () => {
-                    /* noop */
-                },
-            },
-        },
-    }
+    amplifyOutputs
 );
 
 const client = generateClient<Schema>();
