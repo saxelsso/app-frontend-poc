a297bddccf48fac0cf055620af1a5c493deee56f
Stefan Axelsson <26811776+saxelsso@users.noreply.github.com>
2025-09-08 12:33:23 +0200

    fix: correct import path for amplify_outputs.json in echo service handler

--- amplify/functions/echo-service/handler.ts
+++ amplify/functions/echo-service/handler.ts
@@ -2,7 +2,7 @@ import { Amplify } from 'aws-amplify';
 import type { Schema } from "../../data/resource"
 import { env } from "$amplify/env/echoService";
 import { generateClient } from "aws-amplify/data";
-import amplifyOutputs from '../../../amplify_outputs.json';
+import amplifyOutputs from '../amplify_outputs.json';
 
 Amplify.configure(
     amplifyOutputs
